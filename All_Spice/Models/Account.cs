41c62ef42957fe9009881ad2f5dcb770ffb671f3
Object Oriented ProgrammingObject Oriented ProgrammingObject Oriented ProgrammingObject Oriented Programming <back>
2023-08-16 14:29:47 -0600

    first commit

---
+++ All_Spice/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace All_Spice.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
