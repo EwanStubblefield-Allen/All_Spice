7d818daf4da95d3da5b9a059cbdfd4173f5a827c
EwanStubblefield-Allen <ewan2004@yahoo.com>
2023-09-21 15:49:37 -0600

    Testing different port

--- All_Spice.client/src/env.js
+++ All_Spice.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7046' : ''
 export const useSockets = false
 export const domain = 'dev-4sjhjftaw74ytohu.us.auth0.com'
 export const clientId = '1IsKgLLy06BzrKd64KnDD2KSWDzEfuJs'
